4aa46daa97303c0601052911923e6eb47f368fc7
pallavihujare <pallavihujare123@gmail.com>
2026-09-21 17:11:48 +0530

    Create SmartMess home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h1>SmartMess</h1>
+      <p>Hostel Mess & Tiffin Management Platform</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default Home;
